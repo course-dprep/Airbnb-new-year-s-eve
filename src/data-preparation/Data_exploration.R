c3f3690ced3eaf8dd87693e1c70091f45686fa80
Nishtha Stacie <N.j.c.stacie@tilburguniversity.edu>
2023-03-06 13:48:43 +0100

    fix data prep bug: loop now runs and prepares price column well

--- src/data-preparation/Data_exploration.R
+++ src/data-preparation/Data_exploration.R
@@ -1,20 +1,21 @@
-setwd("C:/R/R-4.2.2/bin/Dprep/CounEgAms")
+#setwd("C:/R/R-4.2.2/bin/Dprep/CounEgAms")
 
-list_ams <- read_csv('listings-ams.csv')
-list_lon <- read_csv ('listings-london.csv')
-list_par <- read_csv('listings-paris.csv')
-list_rom <- read_csv('listings-rome.csv')
+# Setup
+library(tidyverse)
 
+# Input
 # Load data sets from the different countries
 filenames = c('listings-ams.csv','listings-london.csv', 'listings-paris.csv','listings-rome.csv')
 
+# Transformation
 lists_cities <- lapply(filenames, function(fn) {
   out = read_csv(fn)
-  out %>% mutate(price_cleaned = as.numeric(gsub('[$]', '', price))) %>% mutate(filenames=fn)
+  out = out %>% mutate(price_cleaned = as.numeric(gsub('[$]', '', price))) %>% mutate(filenames=fn)
   out
-  
-list_cities <- lapply(filenames, process_listings )
-save(lists_cities, file='list_cities.RData')
 })
 
-all_cities <- load('list_cities.RData')
+# Output
+save(lists_cities, file='list_cities.RData')
+
+# Input of your next file
+#all_cities <- load('list_cities.RData')
