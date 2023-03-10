c80272967044d21ee3db78d186d1aec4532f27b6
Fleur Le Mire <fleurlemire@MacBook-Air-van-Fleur.local>
2023-03-10 17:02:29 +0100

    added some codes for graphs

--- src/analysis/Graphs.R
+++ src/analysis/Graphs.R
@@ -1,1 +1,60 @@
+library(broom)
+library(tidyr)
+library(ggplot2)
+library(readr)
+library(data.table)
+library(dplyr)
+library(tidyverse)
+library(stargazer)
+
+##GRAPHS does not make sense yet !!!!!!! 
+
+regression_summary_booked <- summary(model_booked)
+regression_summary_price <- summary(model_price)
+
+London <- read_csv('complete_data_london.csv')
+Paris <- read_csv('complete_data_paris.csv')
+Rome <- read.csv('complete_data_rome.csv')
+Amsterdam <- read_csv('complete_data_ams.csv')
+
+# Visualization #
+# Plot for different cities 
+boxplot_price_per_city <- ggplot(complete_data, aes(x=newyearseve, y=price, fill=city)) + 
+  geom_boxplot() +
+  facet_wrap(~newyearseve, scale="free") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+#create graph with mean price for London
+London_mean_price_graph <- London %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(London$price), color="black")
+
+#create graph with mean price for Paris
+Paris_mean_price_graph <- Paris %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(London$price), color="black")
+
+#create graph with mean price for Rome
+Rome_mean_price_graph <- Rome %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(London$price), color="black")
+
+#create graph with mean price for Amsterdam
+Amsterdam_mean_price_graph <- Amsterdam %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') +  
+  geom_hline(yintercept = mean(London$price), color="black")
+
 
