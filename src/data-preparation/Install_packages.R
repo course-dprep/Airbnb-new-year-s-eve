ef95a2aa58d2f68ea3d8db9064f80e909680f8a4
Linda Tsai <t.yiting@tilburguniversity.edu>
2023-03-16 13:27:43 +0100

    rename file

---
+++ src/data-preparation/Install_packages.R
@@ -0,0 +1,9 @@
+## INSTALL PACKAGES ##
+install.packages("readr") 
+install.packages("dplyr") 
+install.packages("ggplot2") 
+install.packages('tidyverse')
+install.packages('ggpubr') 
+install.packages('car')  
+install.packages('scales') 
+install.packages('stargazer')
