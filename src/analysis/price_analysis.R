0d972f34966bc838fad03ce34691003b86415389
Linda Tsai <t.yiting@tilburguniversity.edu>
2023-03-17 12:36:21 +0100

    modify variable names, add interaction of moderator and IV, and include graph output

--- src/analysis/price_analysis.R
+++ src/analysis/price_analysis.R
@@ -7,8 +7,7 @@ library(dplyr)
 library(tidyverse)
 library(stargazer)
 
-##INPUT##
-#import the complete data 
+## INPUT ##
 complete_data <- read_csv("../../gen/data-preparation/output/complete_data.csv")
 
 #import the data per cities
@@ -19,25 +18,79 @@ for (city in cities) {
   assign(paste0("complete_data_", city), read_csv((paste0("../../gen/data-preparation/output/", "complete_data_",city, ".csv"))))
 }
 
-##TRANSFORMATION##
-#complete model price: linear regression 
-model_price <- lm(price ~ newyearseve, data = complete_data)
-summary(model_price)
+## TRANSFORMATION ##
+# Linear regression with main effect:
+price_main <- lm(price ~ newyearseve, data = complete_data)
+summary(price_main)
 
 
-#Model per city price: linear regression
+# Model per city price: linear regression
 # Loop through each city and generate the model and summary for price
-models_price <- lapply(cities, function(city) {
-  model_price <- lm(price ~ newyearseve, data = get(paste0("complete_data_", city)))
+price_cities <- lapply(cities, function(city) {
+  price_main <- lm(price ~ newyearseve, data = get(paste0("complete_data_", city)))
   cat("Summary for", toupper(city), "\n")
-  print(summary(model_price))
-  return(list(city=city, model_price=model_price))
+  print(summary(price_main))
+  return(list(city=city, price_main=price_main))
 })
 
-#model where differences in prices are shown per city compared to london
-model_price_city_differences <- lm(price ~ newyearseve + paris_dum + rome_dum + ams_dum, data = complete_data)
-summary(model_price_city_differences)
+# Add interactions between new years eve and cities:
+price_cities2 <- lm(price ~ newyearseve + london_dum + paris_dum + ams_dum + rome_dum, data = complete_data); 
+summary(price_cities2)
+
+# model where differences in prices are shown per city compared to london
+price_city_differences <- lm(price ~ newyearseve + paris_dum + rome_dum + ams_dum, data = complete_data)
+summary(price_city_differences )
+
+## Visualization ##
+# Plot for different cities 
+boxplot_price_per_city <- ggplot(complete_data, aes(x=newyearseve, y=price, fill=city)) + 
+  geom_boxplot() +
+  facet_wrap(~newyearseve, scale="free") +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank())
+
+#create graph with mean price for London
+london_mean_price_graph <- complete_data_london %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(complete_data_london$price), color="black")
+
+#create graph with mean price for Paris
+paris_mean_price_graph <- complete_data_paris %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(complete_data_paris$price), color="black")
+
+#create graph with mean price for Rome
+rome_mean_price_graph <- complete_data_rome %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') + 
+  geom_hline(yintercept = mean(complete_data_rome$price), color="black")
+
+#create graph with mean price for Amsterdam
+ams_mean_price_graph <- complete_data_ams %>% 
+  group_by(date) %>%
+  summarize(mean_price = mean(price)) %>%
+  ggplot(aes(x = date, y = mean_price)) + 
+  geom_line(color = 'blue') +  
+  geom_hline(yintercept = mean(complete_data_ams$price), color="black")
+
 
 ##OUTPUT##
-save(model_booked, model_price, models_price, models_booked, model_price_city_differences, file='../../gen/analysis/output/model_results.RData') 
+cities <- c("rome", "paris", "ams", "london")
+
+for (city in cities) {
+plot_obj <- get(paste0(city,"_mean_price_graph"))
+file_name <- paste0(city,"_mean_price_graph", ".pdf")
+file_path <- paste("../../gen/analysis/output/", file_name, sep="")
+ggsave(file_path, plot = plot_obj)
+}
+ggsave(plot = boxplot_price_per_city, filename = "../../gen/analysis/output/boxplot_price_per_city.pdf")
+save(price_main, price_cities, price_cities2, price_city_differences, file='../../gen/analysis/output/model_results.RData') 
 
