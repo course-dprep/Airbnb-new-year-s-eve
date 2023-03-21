920b469472dbd423a2b675e67b07fa3e08cabb61
Linda Tsai <t.yiting@tilburguniversity.edu>
2023-03-22 00:58:07 +0100

    optimize output file's name and set output graph's size

--- src/analysis/price_analysis.R
+++ src/analysis/price_analysis.R
@@ -24,7 +24,6 @@ for (city in cities) {
 summary(complete_data$price)
 mean_price <- mean(complete_data$price)
 save(mean_price, file='../../gen/analysis/output/mean_price.RData')
-table(complete_data$price)
 histogram_prices <- hist(complete_data$price, xlab = 'price in €',ylab = 'numbers of Airbnb') 
 
 # Descriptive of new years eve
@@ -37,22 +36,21 @@ complete_data_sample <- rnorm(5000)
 shapiro.test(complete_data_sample)
 
 # Linear regression with main effect:
-price_main <- lm(price ~ newyearseve, data = complete_data)
-summary(price_main)
-save(price_main, file='../../gen/analysis/output/price_main.RData')
+price_linear <- lm(price ~ newyearseve, data = complete_data)
+summary(price_linear)
 
 # Model per city price: linear regression
 # Loop through each city and generate the model and summary for price
-price_cities <- lapply(cities, function(city) {
-  price_main <- lm(price ~ newyearseve, data = get(paste0("complete_data_", city)))
+price_cities_linear <- lapply(cities, function(city) {
+  price_linear <- lm(price ~ newyearseve, data = get(paste0("complete_data_", city)))
   cat("Summary for", toupper(city), "\n")
-  print(summary(price_main))
-  return(list(city=city, price_main=price_main))
+  print(summary(price_linear))
+  return(list(city=city, price_linear=price_linear))
 })
 
 # Add interactions between new years eve and cities:
-price_cities2 <- lm(price ~ newyearseve + london_dum + paris_dum + ams_dum + rome_dum, data = complete_data); 
-summary(price_cities2)
+price_cities_linear2 <- lm(price ~ newyearseve + london_dum + paris_dum + ams_dum + rome_dum, data = complete_data); 
+summary(price_cities_linear2)
 
 ## Visualization ##
 # Plot for overall
@@ -66,6 +64,7 @@ price_per_city_boxplot <- ggplot(complete_data, aes(x=newyearseve, y=price, fill
   facet_wrap(~newyearseve, scale="free") +
   theme(axis.text.x=element_blank(),
         axis.ticks.x=element_blank())
+
 
 # Create graph with mean price for London
 london_mean_price_graph <- complete_data_london %>% 
@@ -107,8 +106,8 @@ for (city in cities) {
 plot_obj <- get(paste0(city,"_mean_price_graph"))
 file_name <- paste0(city,"_mean_price_graph", ".pdf")
 file_path <- paste("../../gen/analysis/output/", file_name, sep="")
-ggsave(file_path, plot = plot_obj)
+ggsave(file_path, plot = plot_obj,width=3, height=3)
 }
-ggsave(plot = price_newyearseve_boxplot, filename = "../../gen/analysis/output/price_newyearseve_boxplot.pdf")
-ggsave(plot = price_per_city_boxplot, filename = "../../gen/analysis/output/price_per_city_boxplot.pdf")
-save(price_main, price_cities, price_cities2, file='../../gen/analysis/output/model_results.RData') 
+ggsave(plot = price_newyearseve_boxplot, filename = "../../gen/analysis/output/price_newyearseve_boxplot.pdf",width=5, height=5)
+ggsave(plot = price_per_city_boxplot, filename = "../../gen/analysis/output/price_per_city_boxplot.pdf",width=5, height=5)
+save(price_linear, price_cities_linear, price_cities_linear2, file='../../gen/analysis/output/model_results.RData') 
