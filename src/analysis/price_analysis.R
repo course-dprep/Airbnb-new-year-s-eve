9257db41ee8a88e50b58fcb1c72eb376c2eab6b5
Linda Tsai <t.yiting@tilburguniversity.edu>
2023-03-23 13:51:27 +0100

    include sampling process

--- src/analysis/price_analysis.R
+++ src/analysis/price_analysis.R
@@ -7,41 +7,37 @@ library(dplyr)
 library(tidyverse)
 library(stargazer)
 
-## INPUT ##
-complete_data <- read_csv("../../gen/data-preparation/output/complete_data.csv")
-
-# Import the data per cities
-cities <- c("rome", "paris", "ams", "london")
-
-for (city in cities) {
-  file_name <- paste0("complete_data_", city, ".csv")
-  assign(paste0("complete_data_", city), read_csv((paste0("../../gen/data-preparation/output/", "complete_data_",city, ".csv"))))
-}
-
 ## TRANSFORMATION ##
+# Random sampling 10% of dataset#
+sample_data <- complete_data[sample(nrow(complete_data),83924),]
+
+sample_size_london <- round(0.1 * nrow(complete_data_london))
+sample_data_london <- complete_data_london[sample(nrow(complete_data_london), sample_size_london),]
+sample_size_paris <- round(0.1 * nrow(complete_data_paris))
+sample_data_paris <- complete_data_paris[sample(nrow(complete_data_paris), sample_size_paris),]
+sample_size_ams <- round(0.1 * nrow(complete_data_ams))
+sample_data_ams <- complete_data_ams[sample(nrow(complete_data_ams), sample_size_ams),]
+sample_size_rome <- round(0.1 * nrow(complete_data_rome))
+sample_data_rome <- complete_data_rome[sample(nrow(complete_data_rome), sample_size_rome),]
+
 # Descriptive of price
-summary(complete_data$price)
-mean_price <- mean(complete_data$price)
+summary(sample_data$price)
+mean_price <- mean(sample_data$price)
 save(mean_price, file='../../gen/analysis/output/mean_price.RData')
-histogram_prices <- hist(complete_data$price, xlab = 'price in €',ylab = 'numbers of Airbnb') 
+histogram_prices <- hist(sample_data$price, xlab = 'price in €',ylab = 'numbers of Airbnb') 
 
 # Descriptive of new years eve
-summary(complete_data$newyearseve)
-newyearseve_number <- table(complete_data$newyearseve)
-
-# Check normality 
-set.seed(5000)
-complete_data_sample <- rnorm(5000)
-shapiro.test(complete_data_sample)
+summary(sample_data$newyearseve)
+newyearseve_number <- table(sample_data$newyearseve)
 
 # Linear regression with main effect:
-price_linear <- lm(price ~ newyearseve, data = complete_data)
+price_linear <- lm(price ~ newyearseve, data = sample_data)
 summary(price_linear)
 
 # Model per city price: linear regression
 # Loop through each city and generate the model and summary for price
 price_cities_linear <- lapply(cities, function(city) {
-  price_linear <- lm(price ~ newyearseve, data = get(paste0("complete_data_", city)))
+  price_linear <- lm(price ~ newyearseve, data = get(paste0("sample_data_", city)))
   cat("Summary for", toupper(city), "\n")
   print(summary(price_linear))
   return(list(city=city, price_linear=price_linear))
@@ -53,12 +49,12 @@ summary(price_cities_linear2)
 
 ## Visualization ##
 # Plot for overall
-price_newyearseve_boxplot <- ggboxplot(data = complete_data, x="newyearseve", y="price",
+price_newyearseve_boxplot <- ggboxplot(data = sample_data, x="newyearseve", y="price",
                                          color="newyearseve", palette = c("#00AFBB", "#E7B800"),
                                          ylab= "Price", xlab="newyearseve")
 
 # Plot for different cities 
-price_per_city_boxplot <- ggplot(complete_data, aes(x=newyearseve, y=price, fill=city)) + 
+price_per_city_boxplot <- ggplot(sample_data, aes(x=newyearseve, y=price, fill=city)) + 
   geom_boxplot() +
   facet_wrap(~newyearseve, scale="free") +
   theme(axis.text.x=element_blank(),
@@ -105,8 +101,8 @@ for (city in cities) {
 plot_obj <- get(paste0(city,"_mean_price_graph"))
 file_name <- paste0(city,"_mean_price_graph", ".pdf")
 file_path <- paste("../../gen/analysis/output/", file_name, sep="")
-ggsave(file_path, plot = plot_obj,width=3, height=3)
+ggsave(file_path, plot = plot_obj,width=2.25, height=2.25)
 }
-ggsave(plot = price_newyearseve_boxplot, filename = "../../gen/analysis/output/price_newyearseve_boxplot.pdf",width=5, height=5)
-ggsave(plot = price_per_city_boxplot, filename = "../../gen/analysis/output/price_per_city_boxplot.pdf",width=5, height=5)
+ggsave(plot = price_newyearseve_boxplot, filename = "../../gen/analysis/output/price_newyearseve_boxplot.pdf",width=3.5, height=3.5)
+ggsave(plot = price_per_city_boxplot, filename = "../../gen/analysis/output/price_per_city_boxplot.pdf",width=3.5, height=3.5)
 save(price_linear, price_cities_linear, price_cities_linear2, file='../../gen/analysis/output/model_results.RData') 
